b447d710edf5f219498aaf2309ec5b0f6da37312
Matt Choplin <matt.choplin@iconsolutions.com>
2017-05-31 08:49:25 +0100

    Session 13

--- exercises/flask_hello_world.py
+++ exercises/flask_hello_world.py
@@ -1,6 +1,7 @@
 # ---- Flask Hello World ---- #
 # import the Flask class from the flask package
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 # create the application object
 app = Flask(__name__)
 # use the decorator pattern to
@@ -12,16 +13,16 @@ def hello_world():
     return "Hello, World!"
 
 @app.route("/integer/<int:value>")
-def	int_type(value):
-    print(value	+ 1)
-    return	"correct"
-@app.route("/float/<float:value>")
-def	float_type(value):
+def int_type(value):
     print(value + 1)
     return "correct"
-#	dynamic	route that accepts	slashes
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+# dynamic route that accepts slashes
 @app.route("/path/<path:value>")
-def	path_type(value):
+def path_type(value):
     print(value)
     return "correct"
 
@@ -32,6 +33,17 @@ def index(name):
     else :
         return "Not Found", 404
 
+def connect_db():
+    return sqlite3.connect("test_database.db")
+
+@app.route('/persons')
+def main():
+    g.db = connect_db()
+    cur = g.db.execute('select FirstName from People')
+    persons = '\n'.join([p[0] for p in cur.fetchall()])
+    g.db.close()
+    return persons
+
 # start the development server using the run() method
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
